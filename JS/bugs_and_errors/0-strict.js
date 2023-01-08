f65fb91c8c80a319d4d51557dde23cd99efa9097
Ochoja <danielochoja@yahoo.com>
2023-01-08 15:46:33 +0100

    coding practice

---
+++ JS/bugs_and_errors/0-strict.js
@@ -0,0 +1,10 @@
+function spotTheProblem1(){
+    "use strict";
+    /*count from 1 to 10*/
+    for (counter = 1; counter <= 10; counter++) {
+	console.log(counter);
+    }
+}
+
+/*error: because "use strict" demands varaible declaration*/
+spotTheProblem1();
