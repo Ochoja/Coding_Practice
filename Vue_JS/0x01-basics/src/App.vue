51d421475eb60f5528ec4b55aafd91706bb6f17c
Ochoja <danielochoja@yahoo.com>
2023-01-24 13:44:00 +0100

    section-2-notes app

--- Vue_JS/0x01-basics/src/App.vue
+++ Vue_JS/0x01-basics/src/App.vue
@@ -1,53 +1,52 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { ref } from "vue";
+const count = ref(0);
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="num">{{ count }}</div>
+    <div class="btn">
+      <button @click="count++" class="add">+</button>
+      <button @click="count--" class="sub">-</button>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="scss">
+main {
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.num {
+  font-size: 3em;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.btn {
+  margin-top: 10px;
+  display: inline-flex;
+  gap: 10px;
+
+  button {
+    color: #fff;
+    font-size: 1.2em;
+    font-weight: 800;
+    width: 3em;
+    height: 2em;
+    border: none;
+    border-radius: 5px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .add {
+    background: green;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .sub {
+    background: red;
   }
 }
 </style>
