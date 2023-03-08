f5f671158e19e286db3201a8d8b4b1c48c11769b
Ochoja <danielochoja@yahoo.com>
2023-03-08 17:17:00 +0100

    added card component

--- Vue_JS/0x03-quiz_app/src/App.vue
+++ Vue_JS/0x03-quiz_app/src/App.vue
@@ -1,22 +1,33 @@
 <script setup>
 import data from "../data.json";
-import { ref } from "vue";
+import Card from "./components/SubjectCard.vue";
+import { ref, watch } from "vue";
 const subjects = ref(data);
+const search = ref(""); //holds result of search
+
+//Watchers
+watch(search, () => {
+  //update subjects value based on search query
+  subjects.value = data.filter((subject) =>
+    subject.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
   <main>
     <header>
       <h1>Quizzes</h1>
-      <input type="search" placeholder="Search.." />
+      <input v-model="search" type="search" placeholder="Search.." />
     </header>
 
     <section class="quizzes">
-      <div class="subject" v-for="subject in subjects" :key="subject.id">
+      <!-- <div class="subject" v-for="subject in subjects" :key="subject.id">
         <img :src="subject.img" alt="" />
         <h2>{{ subject.name }}</h2>
         <div class="num-question">{{ subject.questions.length }} questions</div>
-      </div>
+      </div> -->
+      <Card v-for="subject in subjects" :key="subject.id" />
     </section>
   </main>
 </template>
@@ -50,27 +61,5 @@ header {
   display: flex;
   gap: 25px;
   flex-wrap: wrap;
-
-  .subject {
-    display: flex;
-    flex-direction: column;
-    width: 250px;
-    box-shadow: rgba(32, 29, 29, 0.1) 0px 4px 12px;
-    border-radius: 5px;
-    cursor: pointer;
-
-    img {
-      border-radius: 5px 5px 0 0;
-    }
-
-    h2 {
-      margin: 8px 0 0 0;
-      padding: 0px 8px;
-    }
-
-    .num-question {
-      padding: 0 0 8px 8px;
-    }
-  }
 }
 </style>
---
+++ Vue_JS/0x03-quiz_app/src/components/SubjectCard.vue
@@ -0,0 +1,36 @@
+<script></script>
+
+<template>
+  <div class="subject">
+    <img
+      src="https://marvel-b1-cdn.bc0a.com/f00000000026007/resilienteducator.com/wp-content/uploads/2012/11/AdobeStock_60467600_cup.jpg"
+      alt=""
+    />
+    <h2>Name</h2>
+    <div class="num-question">3 questions</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.subject {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  box-shadow: rgba(32, 29, 29, 0.1) 0px 4px 12px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  img {
+    border-radius: 5px 5px 0 0;
+  }
+
+  h2 {
+    margin: 8px 0 0 0;
+    padding: 0px 8px;
+  }
+
+  .num-question {
+    padding: 0 0 8px 8px;
+  }
+}
+</style>
